92f185617b03968850a17012b1da02eaa98f4ca1
RamtinPRG <ramtin.barzkar.3@gmail.com>
2022-03-09 01:56:50 +0330

    exmaple of pub/sub structure of MQTT protocol

---
+++ publisher.py
@@ -0,0 +1,67 @@
+import time
+import sys
+from random import randint
+from threading import Timer
+
+import paho.mqtt.client as mqtt
+import keyboard
+from SharedLibs import *
+
+
+def publishInterval():
+    global done
+    msg = randint(35, 39)
+    logging.info(f"Message: {msg}")
+    client.publish(topic, str(msg), QoS, isRetain)
+    if not done:
+        Timer(1, publishInterval).start()
+
+
+logging.basicConfig(level=logging.info)
+
+brokerAddr = "mqtt.eclipseprojects.io"
+DataManager.Add(brokerAddr, Buffers.B_BrokerAddrress)
+
+clientID = f"Client{time.time()}{randint(0, 10)}"
+DataManager.Add(clientID, Buffers.B_ClientID)
+client = mqtt.Client(clientID, False)
+
+DataManager.CheckoutBuffer(Buffers.B_ClientID)
+DataManager.CheckoutBuffer(Buffers.B_BrokerAddrress)
+
+# call back binding
+client.on_log = CallBacks.on_log
+client.on_connect = CallBacks.on_connect
+client.on_disconnect = CallBacks.on_disconnect
+client.on_publish = CallBacks.on_publish
+client.on_subscribe = CallBacks.on_subscribe
+client.on_message = CallBacks.on_message
+
+SetFlags.Connection(client)
+QoS, isRetain = SetFlags.Publish(PatientStatus.PS_Emergency)
+logging.info("Connecting to Broker...")
+client.connect(brokerAddr, 1883)
+
+client.loop_start()
+
+while not client.connected_flag and not client.bad_connection_flag:
+    print(".", end='', flush=True)
+    time.sleep(0.1)
+if client.bad_connection_flag:
+    client.disconnect()
+    client.loop_stop()
+    sys.exit()
+
+topic = "BodyTemperature"
+DataManager.Add(topic, Buffers.B_Topic)
+
+done = False
+
+publishInterval()
+
+while not keyboard.is_pressed('q'):
+    pass
+
+done = True
+client.disconnect()
+client.loop_stop()
